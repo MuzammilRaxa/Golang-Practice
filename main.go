04953ae7623e1da7f78796028a453d5793061225
MuzammilRaxa <razamuzammil426@gmail.com>
2024-10-21 14:31:06 +0500

    Function returns syntax

--- main.go
+++ main.go
@@ -7,12 +7,20 @@ func main() {
 	var fullGolangCourse = "Watch Go full course"
 	var listItems = []string{shortGolangCourse, fullGolangCourse}
 	prinTasks(listItems)
+	fmt.Println()
+
+	listItems = addTask(listItems, "I'm new one")
+	prinTasks(listItems)
 }
 
 func prinTasks(listItems []string) {
 	fmt.Println("List of my todos:")
-
 	for index, task := range listItems {
 		fmt.Printf("%d. %s\n", index+1, task)
 	}
+}
+
+func addTask(taskItems []string, newTask string) []string {
+	var updatedTaskList = append(taskItems, newTask)
+	return updatedTaskList
 }
