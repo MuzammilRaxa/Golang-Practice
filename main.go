109b8e91b4c8646bd178e9023aea0549eafa4df6
MuzammilRaxa <razamuzammil426@gmail.com>
2024-10-21 12:56:17 +0500

    Variables and simple Loop

--- main.go
+++ main.go
@@ -1,3 +1,15 @@
 package main
 
-Print("Hello World")
+import "fmt"
+
+func main() {
+	var shortGolangCourse = "Watch Go crash course"
+	var fullGolangCourse = "Watch Go full course"
+	var listItems = []string{shortGolangCourse, fullGolangCourse}
+	fmt.Println("List of my todos:")
+
+	for index, task := range listItems {
+		fmt.Printf("%d. %s\n", index+1, task)
+	}
+
+}
