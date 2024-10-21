8bf3372d8c80ab97c7693bc613b6077f8788638b
MuzammilRaxa <razamuzammil426@gmail.com>
2024-10-21 13:56:52 +0500

    Func &  func Paramters

--- main.go
+++ main.go
@@ -6,10 +6,13 @@ func main() {
 	var shortGolangCourse = "Watch Go crash course"
 	var fullGolangCourse = "Watch Go full course"
 	var listItems = []string{shortGolangCourse, fullGolangCourse}
+	prinTasks(listItems)
+}
+
+func prinTasks(listItems []string) {
 	fmt.Println("List of my todos:")
 
 	for index, task := range listItems {
 		fmt.Printf("%d. %s\n", index+1, task)
 	}
-
 }
